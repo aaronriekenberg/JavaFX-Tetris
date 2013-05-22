f3ee566ca72e05e8486ed4d02ba62bbce3aabb65
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2013-05-21 20:14:31 -0500

    Use ImmutableList.

--- src/main/java/org/aaron/javafx/tetris/pieces/RandomPieceFactory.java
+++ src/main/java/org/aaron/javafx/tetris/pieces/RandomPieceFactory.java
@@ -1,30 +1,36 @@
 package org.aaron.javafx.tetris.pieces;
 
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.aaron.javafx.tetris.TetrisConstants;
 import org.aaron.javafx.tetris.TetrisCoordinate;
 
+import com.google.common.collect.ImmutableList;
+
 public class RandomPieceFactory {
 
 	private final Random random = new Random();
 
-	private final ArrayList<TetrisPiece> newPieces = new ArrayList<>();
+	private final List<TetrisPiece> pieces;
 
 	public RandomPieceFactory() {
 		final TetrisCoordinate newPieceCenterCoordinate = TetrisCoordinate.of(
 				0, (TetrisConstants.COLUMNS_SET.size() / 2) - 1);
-		newPieces.add(new SquarePiece(newPieceCenterCoordinate));
-		newPieces.add(new LinePiece(newPieceCenterCoordinate));
-		newPieces.add(new TPiece(newPieceCenterCoordinate));
-		newPieces.add(new LeftZPiece(newPieceCenterCoordinate));
-		newPieces.add(new RightZPiece(newPieceCenterCoordinate));
-		newPieces.add(new LeftLPiece(newPieceCenterCoordinate));
-		newPieces.add(new RightLPiece(newPieceCenterCoordinate));
+
+		final ImmutableList.Builder<TetrisPiece> piecesListBuilder = new ImmutableList.Builder<TetrisPiece>();
+		piecesListBuilder.add(new SquarePiece(newPieceCenterCoordinate));
+		piecesListBuilder.add(new LinePiece(newPieceCenterCoordinate));
+		piecesListBuilder.add(new TPiece(newPieceCenterCoordinate));
+		piecesListBuilder.add(new LeftZPiece(newPieceCenterCoordinate));
+		piecesListBuilder.add(new RightZPiece(newPieceCenterCoordinate));
+		piecesListBuilder.add(new LeftLPiece(newPieceCenterCoordinate));
+		piecesListBuilder.add(new RightLPiece(newPieceCenterCoordinate));
+
+		this.pieces = piecesListBuilder.build();
 	}
 
 	public TetrisPiece createRandomPiece() {
-		return newPieces.get(random.nextInt(newPieces.size()));
+		return pieces.get(random.nextInt(pieces.size()));
 	}
 }
